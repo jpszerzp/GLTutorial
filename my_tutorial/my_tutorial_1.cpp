12a0b3bdeab73fae6675270b01f64c0d86430143
Zhuang Peng <pengzhuang428@gmail.com>
2017-04-04 20:01:31 +0800

    first commit on opengl tutorial

---
+++ my_tutorial/my_tutorial_1.cpp
@@ -0,0 +1,15 @@
+//
+//  my_tutorial_1.cpp
+//  Tutorials
+//
+//  Created by 彭庄 on 04/04/2017.
+//
+//
+
+#include "my_tutorial_1.hpp"
+#include <iostream>
+
+int main ()
+{
+    std::cout << "Hello World!" << std::endl;
+}
---
+++ my_tutorial/my_tutorial_1.hpp
@@ -0,0 +1,14 @@
+//
+//  my_tutorial_1.hpp
+//  Tutorials
+//
+//  Created by 彭庄 on 04/04/2017.
+//
+//
+
+#ifndef my_tutorial_1_hpp
+#define my_tutorial_1_hpp
+
+#include <stdio.h>
+
+#endif /* my_tutorial_1_hpp */
